ac4277347171d9313da209eb0ce5a35668164cdf
Nguyễn Văn Hùng <127747148+QE170074@users.noreply.github.com>
2024-10-28 22:47:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vanhung:12345@localhost/table_hub'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vanhung:230303@127.0.0.1:3306/table_hub'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
